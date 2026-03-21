ea6c49ce023576b8a561acdac326fe7aac80c207
Santiago Ospina <santiago.ospina03@uceva.edu.co>
2026-03-21 13:11:39 -0500

    chore(firebase): configurar auth y emuladores de firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:enhorario/app/app.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  if (kDebugMode) {
+    final emulatorHost =
+        !kIsWeb && defaultTargetPlatform == TargetPlatform.android
+        ? '10.0.2.2'
+        : 'localhost';
+
+    FirebaseFirestore.instance.useFirestoreEmulator(emulatorHost, 8080);
+    await FirebaseAuth.instance.useAuthEmulator(emulatorHost, 9099);
+  }
   runApp(const EnHorarioApp());
 }
