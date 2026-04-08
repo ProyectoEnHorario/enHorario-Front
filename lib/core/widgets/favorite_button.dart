7921db7393a07e2009f15ab1457ca8cd283b90bd
MAICOL_ESTIVEN_VASCO_CARDENAS <michael.vasco01@uceva.edu.co>
2026-04-07 19:48:22 -0500

    feat(widgets): diseñar ícono o botón para marcar favoritos

---
+++ lib/core/widgets/favorite_button.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class FavoriteButton extends StatefulWidget {
+  final bool isFavorite;
+  final ValueChanged<bool> onToggle;
+  final double size;
+
+  const FavoriteButton({
+    super.key,
+    required this.isFavorite,
+    required this.onToggle,
+    this.size = 28.0,
+  });
+
+  @override
+  State<FavoriteButton> createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+
+    _scaleAnimation = TweenSequence<double>([
+      TweenSequenceItem(
+        tween: Tween(begin: 1.0, end: 1.3).chain(CurveTween(curve: Curves.easeOut)),
+        weight: 50,
+      ),
+      TweenSequenceItem(
+        tween: Tween(begin: 1.3, end: 1.0).chain(CurveTween(curve: Curves.easeIn)),
+        weight: 50,
+      ),
+    ]).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handleTap() {
+    _controller.forward(from: 0.0);
+    // Hacemos el toggle del booleano, y llamamos el callback.
+    widget.onToggle(!widget.isFavorite);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      iconSize: widget.size,
+      padding: EdgeInsets.zero,
+      constraints: const BoxConstraints(),
+      splashRadius: 24,
+      onPressed: _handleTap,
+      icon: ScaleTransition(
+        scale: _scaleAnimation,
+        child: Icon(
+          widget.isFavorite ? Icons.favorite : Icons.favorite_border,
+          color: widget.isFavorite ? Colors.red : Theme.of(context).disabledColor,
+        ),
+      ),
+    );
+  }
+}
