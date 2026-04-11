68a0707f069fd10e16adab628a293f854b099984
SANTIAGO GACÍA GRANDA <santiago.garcia09@uceva.edu.co>
2026-04-11 12:22:20 -0500

    ENH-8 fix: actualizar expectativa del test con la UI de HomeScreen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,17 +3,24 @@ import 'package:mocktail/mocktail.dart';
 import 'package:enhorario/app/app.dart';
 import 'package:enhorario/features/notifications/domain/repositories/notification_repository.dart';
 
+import 'package:enhorario/features/notifications/domain/entities/notification_status.dart';
+
 class MockNotificationRepository extends Mock implements NotificationRepository {}
 
 void main() {
   testWidgets('Muestra contenido base de home', (WidgetTester tester) async {
     final mockRepo = MockNotificationRepository();
 
+    when(() => mockRepo.requestStatus())
+        .thenAnswer((_) async => NotificationStatus.granted);
+    when(() => mockRepo.getStatus())
+        .thenAnswer((_) async => NotificationStatus.granted);
+
     await tester.pumpWidget(EnHorarioApp(notificationRepository: mockRepo));
     await tester.pumpAndSettle();
 
-    expect(find.text('EnHorario'), findsOneWidget);
-    expect(find.text('Estado de afluencia'), findsOneWidget);
-    expect(find.text('Banco Central - Sucursal Norte'), findsOneWidget);
+    expect(find.text('EnHorario - Inicio'), findsOneWidget);
+    expect(find.text('Selecciona un modo'), findsOneWidget);
+    expect(find.text('App real'), findsOneWidget);
   });
 }
