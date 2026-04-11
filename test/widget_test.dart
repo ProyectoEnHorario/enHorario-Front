f1f04ee8622a7fd142e65555bd81200fcd22bd5b
SANTIAGO GACÍA GRANDA <santiago.garcia09@uceva.edu.co>
2026-04-11 11:49:15 -0500

    ENH-8 fix: corregir widget_test usando MockNotificationRepository

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,17 +1,15 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:mocktail/mocktail.dart';
 import 'package:enhorario/app/app.dart';
+import 'package:enhorario/features/notifications/domain/repositories/notification_repository.dart';
+
+class MockNotificationRepository extends Mock implements NotificationRepository {}
 
 void main() {
   testWidgets('Muestra contenido base de home', (WidgetTester tester) async {
-    await tester.pumpWidget(const EnHorarioApp());
+    final mockRepo = MockNotificationRepository();
+
+    await tester.pumpWidget(EnHorarioApp(notificationRepository: mockRepo));
     await tester.pumpAndSettle();
 
     expect(find.text('EnHorario'), findsOneWidget);
